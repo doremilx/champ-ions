46a985465c73123ec5a8d795ca6312ebda9d9eb7
doremilx <remilin77184@gmail.com>
2025-12-04 23:27:05 +0100

    ajout de l'image dans nav

--- components/header.js
+++ components/header.js
@@ -3,19 +3,20 @@ class HeaderComponent extends HTMLElement {
     super();
   }
 
-  static get observedAttributes() {}
+  static get observedAttributes() { }
 
   connectedCallback() {
     this.render();
   }
 
   render() {
+    const titleSrc = this.getAttribute('title-src');
     const titleAlt = this.getAttribute('title-alt');
     const titlePage = this.getAttribute('title-page');
     this.innerHTML = `
       <header>
         <a href="/">
-          <img alt="" />
+          <img src="${titleSrc}" alt="" width="48px"/>
           <span class="sr-only">${titleAlt}</span>
         </a>
         <h1> ${titlePage} </h1>
